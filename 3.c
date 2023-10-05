403f0ca32c5e290f564fb747b5c7411731609260
tki2mi <ws130575329@gmail.com>
2023-10-05 20:27:39 +0800

    3

---
+++ 3.c
@@ -0,0 +1,27 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int lengthOfLongestSubstring(char *s) {
+    int temp = 0, max = 0;
+    int letters[128] = {0};
+    for (int i; s[i]; i++) {
+        if ((letters[s[i]] != 0) && (letters[s[i]] > temp)) {
+            temp = letters[s[i]];
+        }
+        if ((i - temp + 1) > max) {
+            max = i - temp + 1;
+        }
+        letters[s[i]] = i + 1;
+    }
+    return max;
+}
+
+int main() {
+    char s[100];
+    fgets(s, 100, stdin);
+    int max = lengthOfLongestSubstring(s);
+    printf("%d", max);
+    return 0;
+}
