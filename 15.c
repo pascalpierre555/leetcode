a193741f58f2185adc58784d6df0800e37a0d11d
tki2mi <ws130575329@gmail.com>
2023-11-10 15:34:07 +0800

    22

--- 15.c
+++ 15.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void merge(int* nums, int l, int r, int m) {
-    int* tmp1 = (int*)malloc((m - l + 1) * sizeof(int));
-    int* tmp2 = (int*)malloc((r - m) * sizeof(int));
+void merge(int *nums, int l, int r, int m) {
+    int *tmp1 = (int *)malloc((m - l + 1) * sizeof(int));
+    int *tmp2 = (int *)malloc((r - m) * sizeof(int));
     for (int i = l; i <= m; i++) {
         tmp1[i - l] = nums[i];
     }
@@ -33,7 +33,7 @@ void merge(int* nums, int l, int r, int m) {
     free(tmp2);
 }
 
-void mergeSort(int* nums, int l, int r) {
+void mergeSort(int *nums, int l, int r) {
     if (l < r) {
         int m = (l + r) / 2;
         mergeSort(nums, l, m);
@@ -42,64 +42,112 @@ void mergeSort(int* nums, int l, int r) {
     }
 }
 
-int** threeSum(int* nums, int numsSize, int* returnSize, int** returnColumnSizes) {
-    int** output = (int**)malloc(512 * sizeof(int*));
+int **threeSum(int *nums, int numsSize, int *returnSize, int **returnColumnSizes) {
+    int **output = (int **)malloc(512 * sizeof(int *));
     mergeSort(nums, 0, numsSize - 1);
-    *returnSize = 0;
-    int l, r;
-    int i1 = 123456, i2 = 123456, i3 = 123456;
-    for (int i = 0; (i < numsSize - 2) && (nums[i] <= 0); i++) {
-        if (nums[i] == i1) {
-            continue;
+    int size = 0;
+    int pos = 0, neg = numsSize - 1;
+    int i1, i2;
+    int k1 = 123456, k2 = 123456, k3 = 123456;
+    for (int i = 0; i < numsSize; i++) { //找到正與負從哪開始
+        if (nums[i] >= 0) {
+            neg = i - 1;
+            break;
         }
-        i2 = 123456;
-        i3 = 123456;
-        l = i + 1;
-        r = numsSize - 1;
-        while (l < r) {
-            if ((nums[i] + nums[l] + nums[r] < 0) || (nums[l] == i2)) {
-                i2 = nums[l];
-                l++;
+    }
+    for (int i = neg + 1; i < numsSize; i++) {
+        if (nums[i] > 0) {
+            pos = i;
+            break;
+        }
+    }
+    for (int i = 1; i < numsSize - 1; i++) {
+        if ((nums[i] == k2) && (i != 1)) {
+            i1 = i - 1;
+            i2 = i + 1;
+            while ((i1 >= 0) && (i2 < numsSize) && (nums[i1] == nums[i])) {
+                if ((nums[i1] == k1) || (nums[i1] + nums[i] + nums[i2] > 0)) {
+                    i1--;
+                }
+                else if ((nums[i2] == k3) || (nums[i1] + nums[i] + nums[i2] < 0)) {
+                    i2++;
+                }
+                else {
+                    size++;
+                    output = (int **)realloc(output, size * sizeof(int *));
+                    output[size - 1] = (int *)malloc(3 * sizeof(int));
+                    output[size - 1][0] = nums[i1];
+                    output[size - 1][1] = nums[i];
+                    output[size - 1][2] = nums[i2];
+                    k1 = nums[i1];
+                    k3 = nums[i2];
+                    i1--;
+                    i2++;
+                }
             }
-            else if ((nums[i] + nums[l] + nums[r] > 0) || (nums[r] == i3)) {
-                i3 = nums[r];
-                r--;
+            while (((i + 1) < numsSize) && (nums[i + 1] == nums[i])) {
+                i++;
+            }
+        }
+        else {
+            k1 = 123456;
+            k2 = nums[i];
+            k3 = 123456;
+            if (nums[i] == 0) {
+                i1 = i - 1;
+                i2 = i + 1;
+            }
+            else if (nums[i] > 0) {
+                i1 = neg;
+                i2 = i + 1;
             }
             else {
-                (*returnSize)++;
-                if (*returnSize % 512 == 0) {
-                    output = realloc(output, ((*returnSize / 512) + 1) * 512 * sizeof(int*));
-                }
-                output[*returnSize - 1] = (int*)malloc(3 * sizeof(int));
-                output[*returnSize - 1][0] = nums[i];
-                output[*returnSize - 1][1] = nums[l];
-                output[*returnSize - 1][2] = nums[r];
-                i2 = nums[l];
-                i3 = nums[r];
-                l++;
-                r--;
+                i1 = i - 1;
+                i2 = pos;
             }
-
+            while ((i1 >= 0) && (i2 < numsSize)) {
+                if ((nums[i1] == k1) || (nums[i1] + nums[i] + nums[i2] > 0)) {
+                    i1--;
+                }
+                else if ((nums[i2] == k3) || (nums[i1] + nums[i] + nums[i2] < 0)) {
+                    i2++;
+                }
+                else {
+                    size++;
+                    if ((size % 512) == 0) {
+                        output = realloc(output, ((size / 512) + 1) * 512 * sizeof(int *));
+                    }
+                    output[size - 1] = (int *)malloc(3 * sizeof(int));
+                    output[size - 1][0] = nums[i1];
+                    output[size - 1][1] = nums[i];
+                    output[size - 1][2] = nums[i2];
+                    k1 = nums[i1];
+                    k3 = nums[i2];
+                    i1--;
+                    i2++;
+                }
+            }
         }
-        i1 = nums[i];
     }
-    (*returnColumnSizes) = (int*)malloc((*returnSize) * sizeof(int));
-    for (int i = 0; i < *returnSize; i++) {
-        returnColumnSizes[0][i] = 3;
+    *returnSize = size;
+    (*returnColumnSizes) = (int *)malloc(size * sizeof(int));
+    for (int i = 0; i < size; i++) {
+        (*returnColumnSizes)[i] = 3;
     }
+
     return output;
 }
 
 int main() {
-    int* numsSize = (int*)malloc(sizeof(int));
+    int *numsSize = (int *)malloc(sizeof(int));
     scanf("%d", numsSize);
-    int* nums = (int*)calloc(*numsSize, sizeof(int));
+    int *nums = (int *)calloc(*numsSize, sizeof(int));
     for (int i = 0; i < *numsSize; i++) {
         scanf("%d", &nums[i]);
     }
-    int* returnSize = (int*)calloc(1, sizeof(int));
-    int** returnColumnSize;
-    int** output = threeSum(nums, *numsSize, returnSize, returnColumnSize);
+    int *returnSize = (int *)calloc(1, sizeof(int));
+    int **returnColumnSize;
+    int **output = threeSum(nums, *numsSize, returnSize, returnColumnSize);
     for (int i = 0; i < *returnSize; i++) {
         printf("[%d, %d, %d]\n", output[i][0], output[i][1], output[i][2]);
     }
--- 22.c
+++ 22.c
@@ -2,64 +2,43 @@
 #include <stdlib.h>
 #include <string.h>
 
-void* gengenPar(char** output, char* tmp, int n, int returnSize) {
-    int returnSize2 = (returnSize + 1) / 3;
-    if (n == 1) {
-        strcat(output[0], "()");
+void *gengenPar(char *input, char **output, int pos, int l, int r, int n, int *returnSize) {
+    if (pos == 2 * n) {
+        input[pos] = 0;
+        output[*returnSize] = (char *)malloc(((2 * n) + 1) * sizeof(char));
+        strcpy(output[*returnSize], input);
+        (*returnSize)++;
         return 0;
     }
-    else {
-        gengenPar(output, tmp, n - 1, returnSize2);
-        memset(tmp, 0, (2 * n + 1) * sizeof(char));
-        strcat(tmp, "(");
-        strcat(tmp, output[0]);
-        strcat(tmp, ")");
-        strcat(output[returnSize - 1], tmp);
-        returnSize--;
-        memset(tmp, 0, (2 * n + 1) * sizeof(char));
-        strcat(output[0], "()");
-        for (int i = 1; i < returnSize2; i++) {
-            memset(tmp, 0, (2 * n + 1) * sizeof(char));
-            strcat(tmp, "()");
-            strcat(tmp, output[i]);
-            strcat(output[returnSize - 1], tmp);
-            returnSize--;
-            memset(tmp, 0, (2 * n + 1) * sizeof(char));
-            strcat(tmp, "(");
-            strcat(tmp, output[i]);
-            strcat(tmp, ")");
-            strcat(output[returnSize - 1], tmp);
-            returnSize--;
-            strcat(output[i], "()");
-        }
+    if (l < n) {
+        input[pos] = '(';
+        gengenPar(input, output, pos + 1, l + 1, r, n, returnSize);
+    }
+    if ((r < n) && (r < l)) {
+        input[pos] = ')';
+        gengenPar(input, output, pos + 1, l, r + 1, n, returnSize);
     }
     return 0;
 }
 
-char** generateParenthesis(int n, int* returnSize) {
+char **generateParenthesis(int n, int *returnSize) {
+    (*returnSize) = 0;
     if (n == 0) {
-        (*returnSize) = 0;
         return 0;
     }
-    (*returnSize) = 1;
-    for (int i = 0; i < n - 1; i++) {
-        (*returnSize) = ((*returnSize) * 3) - 1;
-    }
-    char** output = (char**)malloc((*returnSize) * sizeof(char*));
-    char* tmp = (char*)calloc((2 * n + 1), sizeof(char));
-    for (int i = 0; i < (*returnSize); i++) {
-        output[i] = (char*)calloc((2 * n + 1), sizeof(char));
-    }
-    gengenPar(output, tmp, n, (*returnSize));
+    char **output = (char **)malloc(2048 * sizeof(char *));
+    char *input = (char *)malloc(((2 * n) + 1) * sizeof(char));
+    gengenPar(input, output, 0, 0, 0, n, returnSize);
     return output;
 }
 
 int main() {
-    int* returnSize = malloc(sizeof(int));
+    int *returnSize = malloc(sizeof(int));
     int a;
     scanf("%d", &a);
-    char** output = generateParenthesis(a, returnSize);
+    char **output = generateParenthesis(a, returnSize);
     for (int i = 0; i < (*returnSize); i++) {
         printf("%s\n", output[i]);
     }
+    return 0;
 }
