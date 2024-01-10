744c8e852ecf06c826c583a7926184b7d8dcff25
tki2mi <ws130575329@gmail.com>
2024-01-10 14:45:45 +0800

    117

--- 117.c
+++ 117.c
@@ -8,30 +8,53 @@ struct Node {
     struct Node *next;
 };
 
+void connectParallel(struct Node *root, struct Node *prev) {
+    if (root->left && root->right) {
+        if (prev) {
+            prev->next = root->left;
+        }
+        root->left->next = root->right;
+        prev = root->right;
+    }
+    else if (root->left) {
+        if (prev) {
+            prev->next = root->left;
+        }
+        prev = root->left;
+    }
+    else if (root->right) {
+        if (prev) {
+            prev->next = root->right;
+        }
+        prev = root->right;
+    }
+    if (root->next) {
+        connectParallel(root->next, prev);
+    }
+    return;
+}
+
 struct Node *connect(struct Node *root) {
     if (root == NULL) {
         return NULL;
     }
-    if (root->left && root->right) {
-        root->left->next = root->right;
-    }
-    if (root->next) {
-        if (root->right) {
-            if (root->next->left) {
-                root->right->next = root->next->left;
-            }
-            else if (root->next->right) {
-                root->right->next = root->next->right;
-            }
+    struct Node *output = root;
+    connectParallel(root, NULL);
+    while (1) {
+        if (root->left) {
+            root = root->left;
+            connectParallel(root, NULL);
         }
-        else if (root->left) {
-            if (root->next->left) {
-                root->left->next = root->next->left;
-            }
-            else if (root->next->right) {
-                root->left->next = root->next->right;
-            }
+        else if (root->right) {
+            root = root->right;
+            connectParallel(root, NULL);
         }
-        connect(root->next);
+        else if (root->next) {
+            root = root->next;
+        }
+        else {
+            break;
+        }
     }
+    return output;
 }
